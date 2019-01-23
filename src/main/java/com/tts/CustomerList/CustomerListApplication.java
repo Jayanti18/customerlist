729a0b0e3d3b69711f958eec2d8cf4daba0a1aa6
jayanti18 <jayantirajesh@yahoo.com>
2019-01-23 18:38:43 -0500

    first commit

---
+++ src/main/java/com/tts/CustomerList/CustomerListApplication.java
@@ -0,0 +1,14 @@
+package com.tts.CustomerList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerListApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tts/CustomerList/CustomerListApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tts.CustomerList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
